14f7a4d840be7f81d05ecbe32cf16c52774df839
TDK1969 <793065367@qq.com>
2021-08-12 10:19:46 +0800

    8.11打卡

---
+++ Dynamic Programming/446.arithmetic-slices-ii-subsequence.py
@@ -0,0 +1,24 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        d = [dict() for _ in range(n)]
+        count = 0
+
+        for i in range(1, n):
+            for j in range(i):
+                delta = nums[i] - nums[j]
+                if delta in d[i]:
+                    d[i][delta] += 1
+                else:
+                    d[i][delta] = 1
+
+                if delta in d[j]:
+                    d[i][delta] += d[j][delta]
+                    count += d[j][delta]
+        return count
+
+test = Solution()
+print(test.numberOfArithmeticSlices([2,4,6,8,10]))

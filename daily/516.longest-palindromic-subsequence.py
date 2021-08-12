3c6cc064310b43eda8ad0cac162aaffa38313919
TDK1969 <793065367@qq.com>
2021-08-12 17:33:45 +0800

    8.12打卡 微调

--- daily/516.longest-palindromic-subsequence.py
+++ daily/516.longest-palindromic-subsequence.py
@@ -2,7 +2,7 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         n = len(s)
         dp = [[0 for _ in range(n)] for _ in range(n)]
-        ans = 1  # 注意长度为1的情况，所以ans初值为1
+
         for i in range(n):
             dp[i][i] = 1
         for length in range(2, n + 1):
@@ -14,8 +14,8 @@ class Solution:
                     dp[l][r] = max(dp[l + 1][r], dp[l][r - 1])
                 else:
                     dp[l][r] = max(dp[l + 1][r], dp[l][r - 1], dp[l + 1][r - 1] + 2)
-                ans = max(ans, dp[l][r])
-        return ans
+
+        return dp[0][-1]
 
 test = Solution()
 print(test.longestPalindromeSubseq("cbbd"))

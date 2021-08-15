9098c1042ee1421b4737684b180de2e009de26e9
TDK's Y7000p <793065367@qq.com>
2021-08-15 17:37:07 +0800

    快速幂

---
+++ useful_function/Fast Power.py
@@ -0,0 +1,9 @@
+def quick_algorithm(a, b, c):
+    a = a % c
+    ans = 1
+    while b != 0:
+        if b & 1:
+            ans = (ans * a) % c
+        b >>= 1
+        a = (a * a) % c
+    return ans

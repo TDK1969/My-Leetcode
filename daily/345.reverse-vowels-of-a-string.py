dd549d1b498cdd100eaedf6af5878d802c51c455
TDK's Y7000p <793065367@qq.com>
2021-08-19 10:10:35 +0800

    8.19打卡成功

---
+++ daily/345.reverse-vowels-of-a-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        n = len(s)
+        l = 0
+        r = n - 1
+        vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+        s = list(s)
+        while l < r:
+            while l < n and s[l] not in vowels:
+                l += 1
+            while r >= 0 and s[r] not in vowels:
+                r -= 1
+            if l < r:
+                s[l], s[r] = s[r], s[l]
+                l += 1
+                r -= 1
+        return "".join(s)
+
+test = Solution()
+print(test.reverseVowels("leetcode"))

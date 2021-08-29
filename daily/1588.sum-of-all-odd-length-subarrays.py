d666ef79458cf3f83b77c2111e1a45311b887f73
TDK's Y7000p <793065367@qq.com>
2021-08-29 09:55:45 +0800

    8.20打卡

---
+++ daily/1588.sum-of-all-odd-length-subarrays.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def sumOddLengthSubarrays(self, arr: List[int]) -> int:
+        presums = [0]
+        presum = 0
+        for num in arr:
+            presum += num
+            presums.append(presum)
+        n = len(arr)
+        max_len = n if n & 1 else n - 1
+        ans = 0
+        for i in range(1, max_len + 1, 2):
+            for start in range(0, len(presums) - i):
+                ans += presums[start + i] - presums[start]
+        return ans
+
+test = Solution()
+print(test.sumOddLengthSubarrays([1]))

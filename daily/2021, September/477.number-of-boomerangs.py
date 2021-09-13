6982584d8519f949264e361812605e0ecd126bcf
TDK's Y7000p <793065367@qq.com>
2021-09-13 14:10:06 +0800

    9.13打卡

---
+++ daily/2021, September/477.number-of-boomerangs.py
@@ -0,0 +1,16 @@
+from typing import List
+from collections import defaultdict
+
+class Solution:
+    def numberOfBoomerangs(self, points: List[List[int]]) -> int:
+        ans = 0
+        for point in points:
+            distances = defaultdict(int)
+            for next_point in points:
+                d = (point[0] - next_point[0]) ** 2 + (point[1] - next_point[1]) ** 2
+                distances[d] += 1
+                ans += 2 * (distances[d] - 1)
+        return ans
+
+test = Solution()
+print(test.numberOfBoomerangs([[0,0],[1,0],[2,0]]))

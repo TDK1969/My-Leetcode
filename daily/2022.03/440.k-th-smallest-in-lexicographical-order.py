cab8d00c91aba1cafcf73202dc56eea78e6da62a
TDK <793065367@qq.com>
2022-03-23 20:38:26 +0800

    3.23打卡

---
+++ daily/2022.03/440.k-th-smallest-in-lexicographical-order.py
@@ -0,0 +1,26 @@
+"""
+日期: 2022-03-23
+题目: 字典序的第K小数字
+链接: https://leetcode-cn.com/problems/k-th-smallest-in-lexicographical-order/
+"""
+class Solution:
+    def getSteps(self, cur: int, n: int) -> int:
+        steps, first, last = 0, cur, cur
+        while first <= n:
+            steps += min(last, n) - first + 1
+            first *= 10
+            last = last * 10 + 9
+        return steps
+
+    def findKthNumber(self, n: int, k: int) -> int:
+        cur = 1
+        k -= 1
+        while k:
+            steps = self.getSteps(cur, n)
+            if steps <= k:
+                k -= steps
+                cur += 1
+            else:
+                cur *= 10
+                k -= 1
+        return cur

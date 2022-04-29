def38be1e703d8be640c0547ee9d64b626d9b141
TDK <793065367@qq.com>
2022-04-30 00:22:16 +0800

    4.30打卡 4月全打卡

---
+++ daily/2022.04/908.smallest-range-i.py
@@ -0,0 +1,10 @@
+"""
+日期: 2022-04-30
+题目: 最小差值 I
+链接: https://leetcode-cn.com/problems/smallest-range-i/
+"""
+from typing import List
+
+
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:

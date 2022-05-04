f15c568214620f8ba9e28470ee7d74dac2142871
TDK <793065367@qq.com>
2022-05-04 09:23:19 +0800

    291周赛 重做

--- contest/weekly-contest-291/1.remove-digit-from-number-to-maximize-result.py
+++ contest/weekly-contest-291/1.remove-digit-from-number-to-maximize-result.py
@@ -2,3 +2,12 @@
 题目: 移除指定数字得到的最大结果
 链接: https://leetcode-cn.com/problems/remove-digit-from-number-to-maximize-result/
 """
+class Solution:
+    def removeDigit(self, number: str, digit: str) -> str:
+        ans = 0
+        for i in range(len(number)):
+            num = number[i]
+            if num == digit:
+                temp = number[:i] + number[i + 1:]
+                ans = max(ans, int(temp))
+        return str(ans)
--- contest/weekly-contest-291/2.minimum-consecutive-cards-to-pick-up.py
+++ contest/weekly-contest-291/2.minimum-consecutive-cards-to-pick-up.py
@@ -2,3 +2,20 @@
 题目: 必须拿起的最小连续卡牌数
 链接: https://leetcode-cn.com/problems/minimum-consecutive-cards-to-pick-up/
 """
+from typing import List
+from collections import defaultdict
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        card_index = {}
+        min_diff = float("inf")
+        for index, card in enumerate(cards):
+            if card not in card_index:
+                card_index[card] = index
+            else:
+                min_diff = min(index - card_index[card] + 1, min_diff)
+                card_index[card] = index
+        if min_diff == float("inf"):
+            return -1
+        return min_diff
+
+            

eef6b6ddb74f04a6d6cebcfa69dd6f52bce67197
TDK <793065367@qq.com>
2022-05-26 13:56:20 +0800

    5.22打卡

---
+++ daily/2022.05/464.can-i-win.py
@@ -0,0 +1,10 @@
+"""
+日期: 2022-05-22
+题目: 我能赢吗
+链接: https://leetcode-cn.com/problems/can-i-win/
+"""
+
+from typing import *
+from collections import *
+class Solution:
+    def canIWin(self, maxChoosableInteger: int, desiredTotal: int) -> bool:

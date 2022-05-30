ce91c1ec091ca09b3c5024b72b69b0f357c3567f
TDK <793065367@qq.com>
2022-05-30 20:20:49 +0800

    5.26

---
+++ daily/2022.05/699.falling-squares.py
@@ -0,0 +1,10 @@
+"""
+日期: 2022-05-26
+题目: 掉落的方块
+链接: https://leetcode-cn.com/problems/falling-squares/
+"""
+
+from typing import *
+from collections import *
+class Solution:
+    def fallingSquares(self, positions: List[List[int]]) -> List[int]:

7ffecb96be98020076edd5cac4e9409749fc80d8
TDK <793065367@qq.com>
2022-06-13 21:15:41 +0800

    6.13打卡

---
+++ daily/2022.06/1051.height-checker.py
@@ -0,0 +1,28 @@
+"""
+日期: 2022-06-13
+题目: 高度检查器
+链接: https://leetcode-cn.com/problems/height-checker/
+"""
+
+from typing import *
+from collections import *
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        ans = 0
+        cnt = [0 for _ in range(105)]
+        n = len(heights)
+        for i in heights:
+            cnt[i] += 1
+        idx = 0
+        for i in range(105):
+            while cnt[i]:
+                if i != heights[idx]:
+                    ans += 1
+                cnt[i] -= 1
+                idx += 1
+        
+    
+        return ans
+
+test = Solution()
+print(test.heightChecker([1,1,4,2,1,3]))

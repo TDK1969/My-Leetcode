823323c5af580ae82e79fed5a7e55408e9829bfa
TDK <793065367@qq.com>
2022-07-04 01:02:30 +0800

    7.4打卡

--- daily/2022.07/1200.minimum-absolute-difference.py
+++ daily/2022.07/1200.minimum-absolute-difference.py
@@ -8,3 +8,14 @@ from typing import *
 from collections import *
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        ans = []
+        n = len(arr)
+        m = float("inf")
+        for i in range(1, n):
+            if arr[i] - arr[i - 1] < m:
+                ans = [[arr[i - 1], arr[i]]]
+                m = arr[i] - arr[i - 1]
+            elif arr[i] - arr[i - 1] == m:
+                ans.append([arr[i - 1], arr[i]])
+        return ans
